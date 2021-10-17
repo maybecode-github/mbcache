52efd1186c7baa792b8f7ccb979862a0a38aeb21
Alexander K <maybecode@maybecode.de>
2021-10-17 18:37:36 +0200

    build cache base

---
+++ src/main/java/de/maybecode/mbcache/CacheMap.java
@@ -0,0 +1,37 @@
+package de.maybecode.mbcache;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public interface CacheMap<K, V> {
+
+    void cache(K key, V value);
+
+    void cache(K key, V value, long expireAfter, TimeUnit expirationTimeUnit);
+
+    void unCache(K key);
+
+    void reloadCache(K key, V value);
+
+    Optional<V> findByKey(K key);
+
+    Optional<K> findByValue(V value);
+
+    List<K> findKeys();
+
+    List<V> findValues();
+
+    boolean isCached(K key);
+
+    boolean isCachedValue(V value);
+
+    /**
+     * clears cache completely after time
+     *
+     * @param amount   amount of time after cache gets cleared
+     * @param timeUnit unit of time after cache gets cleared
+     */
+    void expireAfter(long amount, TimeUnit timeUnit);
+
+}
---
+++ src/main/java/de/maybecode/mbcache/cache/MBCache.java
@@ -0,0 +1,77 @@
+package de.maybecode.mbcache.cache;
+
+import de.maybecode.mbcache.CacheMap;
+
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MBCache<K, V> implements CacheMap<K, V> {
+
+    private final Map<K, V> cacheMap = new HashMap<>();
+
+    @Override
+    public void cache(K key, V value) {
+        if (!cacheMap.containsKey(key))
+            cacheMap.put(key, value);
+    }
+
+    @Override
+    public void cache(K key, V value, long expireAfter, TimeUnit expirationTimeUnit) {
+        cache(key, value);
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        Runnable expiration = () -> reloadCache(key, value);
+        executorService.schedule(expiration, expireAfter, expirationTimeUnit);
+        executorService.shutdown();
+    }
+
+    @Override
+    public void unCache(K key) {
+        cacheMap.remove(key);
+    }
+
+    @Override
+    public void reloadCache(K key, V value) {
+        cacheMap.replace(key, value);
+    }
+
+    @Override
+    public Optional<V> findByKey(K key) {
+        return Optional.ofNullable(cacheMap.get(key));
+    }
+
+    @Override
+    public List<K> findKeys() {
+        return new ArrayList<>(cacheMap.keySet());
+    }
+
+    @Override
+    public List<V> findValues() {
+        return new ArrayList<>(cacheMap.values());
+    }
+
+    @Override
+    public Optional<K> findByValue(Object value) {
+        return cacheMap.keySet().stream().filter(mapValue -> mapValue.equals(value)).findFirst();
+    }
+
+    @Override
+    public boolean isCached(K key) {
+        return cacheMap.containsKey(key);
+    }
+
+    @Override
+    public boolean isCachedValue(V value) {
+        return cacheMap.containsValue(value);
+    }
+
+    @Override
+    public void expireAfter(long amount, TimeUnit timeUnit) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        Runnable expiration = cacheMap::clear;
+        executorService.schedule(expiration, amount, timeUnit);
+        executorService.shutdown();
+    }
+
+}
